51c71fa6ab20774bbc6f7a13d540304bafe0aa0f
sachin00 <007sachin.sp@gmail.com>
2023-04-25 00:27:39 +0530

    Added sanitization

--- auth/src/routes/__tests__/signup.test.ts
+++ auth/src/routes/__tests__/signup.test.ts
@@ -1,10 +1,102 @@
 import request from 'supertest';
 import app from '../../app';
+import { SIGNUP_ROUTE } from '../signup';
 
+/**
+ * Valid email conditions:
+ *  -Standard email formats from 'express-validator' package
+ */
+describe('tests validity of email input', () => {
+  let password = '';
 
+  beforeAll(() => {
+    password = 'ValidPassword1';
+  });
 
-it('should return 422 if the email is not valid', async () => {
-  await request(app).post('/api/auth/signup').send({}).expect(422);
+  it('should return 422 if the email is not provided', async () => {
+    await request(app).post(SIGNUP_ROUTE).send({ password }).expect(422);
+  });
 
-  await request(app).post('/api/auth/signup').send({ email: 'invalid email' }).expect(422);
+  it('should return 422 if the email is not valid', async () => {
+    await request(app).post(SIGNUP_ROUTE).send({ email: 'invalidEmail', password }).expect(422);
+  });
+
+  it('should return 200 if the email is valid', async () => {
+    await request(app).post(SIGNUP_ROUTE).send({ email: 'test@tes.com', password }).expect(200);
+  });
+});
+
+/***
+ * Valid password conditions:
+ * - Atleast 8 characters
+ * - Atmost 32 characters
+ * - One lowercase letter
+ * - One uppercase letter
+ * - One digit
+ */
+describe('tests validity of password input', () => {
+  let email = '';
+
+  beforeAll(() => {
+    email = 'test@test.com';
+  });
+
+  it('should return 422 if the password is not provided', async () => {
+    await request(app).post(SIGNUP_ROUTE).send({ email }).expect(422);
+  });
+
+  it('should return 422 if the password contains less than 8 characters', async () => {
+    await request(app).post(SIGNUP_ROUTE).send({ email, password: 'Valid12' }).expect(422);
+  });
+
+  it('should return 422 if the password contains more than 32 characters', async () => {
+    await request(app)
+      .post(SIGNUP_ROUTE)
+      .send({ email, password: 'Valid12Valid12Valid12Valid12Valid12Valid12' })
+      .expect(422);
+  });
+
+  it('should return 422 if the password does not contain lowercase character', async () => {
+    await request(app).post(SIGNUP_ROUTE).send({ email, password: 'VALID12345' }).expect(422);
+  });
+
+  it('should return 422 if the password does not contain uppercase character', async () => {
+    await request(app).post(SIGNUP_ROUTE).send({ email, password: 'valid1234' }).expect(422);
+  });
+
+  it('should return 422 if the password does not contain a digit', async () => {
+    await request(app).post(SIGNUP_ROUTE).send({ email, password: 'ValidValid' }).expect(422);
+  });
+
+  it('should return 200 if the password is valid', async () => {
+    await request(app).post(SIGNUP_ROUTE).send({ email, password: 'ValidValid12' }).expect(200);
+  });
+});
+
+describe('tests sanitization of email input', () => {
+  it('should not contain uppercase letters in the domain of the email', async () => {
+    const normalizeEmail = 'test@test.com';
+
+    const response = await request(app)
+      .post(SIGNUP_ROUTE)
+      .send({
+        email: 'test@TEST.com',
+        password: 'Valid123'
+      })
+      .expect(200);
+
+    expect(response.body.email).toEqual(normalizeEmail);
+  });
+});
+
+describe('tests sanitization of password input', () => {
+  it('should not contain unescaped characters', async () => {
+    await request(app)
+      .post(SIGNUP_ROUTE)
+      .send({
+        email: 'test@test.com',
+        password: 'Valid1<>'
+      })
+      .expect(200);
+  });
 });
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,20 +1,45 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
-import { Request, Response } from 'express';
+
+export const SIGNUP_ROUTE = '/api/auth/signup';
 
 const signUpRouter = express.Router();
 
 signUpRouter.post(
-  '/api/auth/signup',
-  [body('email').isEmail().withMessage('Email must be in valid format...')],
+  SIGNUP_ROUTE,
+  [
+    body('email').isEmail().withMessage('Email must be in valid format...').normalizeEmail(),
+    body('password')
+      .trim()
+      .isLength({ min: 8, max: 32 })
+      .withMessage('Password must be between 8 and 32 characters...'),
+    body('password')
+      .matches(/^(.*[a-z].*)$/)
+      .withMessage('Password must contain atleast one lowercase letter'),
+    body('password')
+      .matches(/^(.*[A-Z].*)$/)
+      .withMessage('Password must contain atleast one uppercase letter'),
+    body('password')
+      .matches(/^(.*\d.*)$/)
+      .withMessage('Password must contain atleast one digit'),
+    body('password').escape()
+  ],
   (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      res.status(422).send({});
+      res.sendStatus(422);
     }
 
-    res.send({});
+    if (/.+@[A-Z]/g.test(req.body.email)) {
+      res.sendStatus(422);
+    }
+
+    if (/[><'*/]/g.test(req.body.password)) {
+      res.sendStatus(422);
+    }
+
+    res.send({ email: req.body.email });
   }
 );
 
